1601daab2bd211c106a073b9583251bece8704a4
Xu Xian <xumonkey1992@gmail.com>
2023-12-23 13:22:36 -0800

    Update resource

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 // import { HookSample2 } from './components/HookSamples';
-// import Matrix from './components/Matrix';
+import Matrix from './components/Matrix';
 import Physical from './components/Pysical';
 
 function App() {
   return (
     <div className="App">
-      <Physical />
+      <Matrix />
     </div>
   );
 }
